ef583ecb73ee02335897a2d2b0ef2f7fc0729373
Michael Laenen <michael.laenen@agilos.com>
2023-03-08 08:40:26 +0100

    change header favorites

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('Mica\'s new Healthy diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
