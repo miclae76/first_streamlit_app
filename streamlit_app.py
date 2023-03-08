a3f8f9f94ac5f198edcb6434197472bfc0ca9c5d
Michael Laenen <michael.laenen@agilos.com>
2023-03-08 08:36:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('Mica\'s new Healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.footer ('Copyright MLA')
+
